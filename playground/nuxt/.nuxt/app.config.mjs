73216ca55932888600c36888fb8f106e0ed0c42c
nWacky <gosha.evtushenko@gmail.com>
2023-08-18 20:20:42 +0300

    remove .nuxt

--- playground/nuxt/.nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/Users/dev/Documents/devise-token-auth-vue/playground/nuxt/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
